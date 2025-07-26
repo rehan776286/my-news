4ab051f6c60edcf6fd50109543fcf6b7518413ea
Rehan Khan <rehankhan2mrk@gmail.com>
2025-07-26 16:10:11 +0530

    newFix

--- src/components/header.js
+++ src/components/header.js
@@ -6,19 +6,19 @@ const Header = () => {
   return (
     <header className="w-full bg-gray-800 ">
       <div className="flex justify-between items-center py-1 px-3">
-        {/* <img
+        <img
           src="/head.png"
-          alt=""
+          alt="trendmode news"
           className="w-30 md:w-40 h-auto object-contain select-none pointer-events-none"
-        /> */}
-        <Image
+        />
+        {/* <Image
           src="https://res.cloudinary.com/dq6s0fy8k/image/upload/v1753525870/head_fdhhzg.png"
           alt="Share on WhatsApp"
           width={200}
           height={160}
           className="w-30 md:w-40 h-auto object-contain select-none pointer-events-none"
           priority
-        />
+        /> */}
         <LanguageToggleButton />
       </div>
       {/* <Navbar /> */}
